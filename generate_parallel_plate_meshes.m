4f62e55fcb18c6215765d15f9f523aa0f935a22b
NatanHerzog <natanherzog@gmail.com>
2025-03-12 00:45:03 -0400

    new meshes for parallel plates

--- generate_parallel_plate_meshes.m
+++ generate_parallel_plate_meshes.m
@@ -4,11 +4,11 @@ clear all
 close all
 clc
 
-addpath("submodules\distmesh-utilities\")
+addpath("submodules/distmesh-utilities/")
 
-output_folder = "parallel-plates\\";
+output_folder = "parallel-plates/";
 
-scale_factors = [2, 5, 10, 20, 30, 50];
+scale_factors = [2, 5, 10, 20, 50, 80];
 separation = 1;
 side_lengths = [0.1, 0.2, 0.4, 0.6, 1.0, 2.0, 4.0, 10.0];
 bottom_c = [ 1,2,3; 1,3,4 ];
@@ -16,7 +16,7 @@ top_c = [ 1,3,2; 1,4,3 ];
 
 
 for i = 1 : length(scale_factors)
-  folder_name = output_folder + "L" + num2str(i) + "\\";
+  folder_name = output_folder + "L" + num2str(i) + "/";
 
   for l = side_lengths
     p = [ 0,0,0; l,0,0; l,l,0; 0,l,0 ];
@@ -24,7 +24,7 @@ for i = 1 : length(scale_factors)
     tM = translateMesh( bM, [0,0,separation] );
     tM = flipNormals( tM );
     bottom_name = "bottom-xDim=" + num2str(l) + "-yDim=" + num2str(l) + ".stl";
-    top_name = "top-xDim=" + num2str(l) + "-yDim=" + num2str(l) + "sep=" + num2str(separation) + ".stl";
+    top_name = "top-xDim=" + num2str(l) + "-yDim=" + num2str(l) + "-sep=" + num2str(separation) + ".stl";
   
     stlwrite(bM, folder_name + bottom_name, "binary")
     stlwrite(tM, folder_name + top_name, "binary")
