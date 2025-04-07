f5ea1297655366c466cdeb2e4b4d9bb74fdf8880
Natan Herzog <collegenatan@gmail.com>
2025-04-07 15:14:45 -0400

    updating all circle meshes

--- generate_parallel_disc_meshes.m
+++ generate_parallel_disc_meshes.m
@@ -8,17 +8,17 @@ addpath("submodules/distmesh-utilities/")
 
 output_folder = "coaxial-discs/";
 
-scale_factors = [2, 5, 10, 20, 50, 80];
+num_divisions = [16, 32, 64, 128, 256, 512];
 offset = 1.0;
 radii = [0.2, 0.8, 1.0, 2.0, 5.0, 8.0, 10.0];
 
-for i = 5 : length(scale_factors)
+for i = 1 : length(num_divisions)
   folder_name = output_folder + "L" + num2str(i) + "/";
 
   for j = 1 : length(radii)
     r = radii(j);
 
-    bM = generateDisc( r, scale_factors(i) );
+    bM = generateCircle( r, num_divisions(i) );
     tM = translateMesh( bM, [0,0,offset] );
     tM = flipNormals( tM );
     bottom_name = "bottom-r=" + num2str(r) + ".stl";
